6e250ea2f45ac047bc80fd1332e072da4f86df7e
mimi <zhu.emily@northeastern.edu>
2026-03-29 22:25:45 -0400

    e

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import Hello from "./Hello.js"
+import Lab5 from './lab5/index.js'
+import cors from "cors";
+import session from "express-session";
+import db from "./kambaz/database/index.js";
+import UserRoutes from "./kambaz/users/routes.js";
+import CourseRoutes from "./kambaz/courses/routes.js";
+import "dotenv/config";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.SERVER_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.SERVER_URL,
+  };
+}
+app.use(session(sessionOptions));
+
+app.use(express.json());
+UserRoutes(app, db);
+CourseRoutes(app, db);
+Hello(app)
+Lab5(app)
+app.listen(process.env.PORT || 4000)
---
+++ lab5/index.js
@@ -0,0 +1,15 @@
+import PathParameters from "./PathParameters.js";
+import QueryParameters from "./QueryParameters.js";
+import WorkingWithObjects from "./WorkingWithObjects.js";
+import Module from "./Module.js";
+import WorkingWithArrays from "./WorkingWithArrays.js";
+export default function Lab5(app) {
+  app.get("/lab5/welcome", (req, res) => {
+    res.send("Welcome to Lab 5");
+  });
+  PathParameters(app);
+  WorkingWithObjects(app);
+  QueryParameters(app);
+  Module(app);
+  WorkingWithArrays(app);
+};
