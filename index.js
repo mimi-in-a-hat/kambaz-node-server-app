4293e4a8bd887a0845c8ee8e35c38f515a5b646e
mimi <zhu.emily@northeastern.edu>
2026-04-13 23:25:03 -0400

    help

--- index.js
+++ index.js
@@ -26,11 +26,12 @@ const sessionOptions = {
   saveUninitialized: false,
 };
 if (process.env.SERVER_ENV !== "development") {
+  app.set("trust proxy", 1);
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.SERVER_URL,
+    httpOnly: true,
   };
 }
 app.use(session(sessionOptions));
